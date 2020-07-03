0d49aa56195b4228b050ed34eb1717a767a9cfc3
chan <thechanmoon@gmail.com>
2020-07-03 00:13:31 -0400

    Loading added

--- App.js
+++ App.js
@@ -1,21 +1,54 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import Loading from "./Loading";
+
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <Loading> </Loading>
+    // <View style={styles.container}>
+    //   <Text style={styles.text}>Hi JW!!</Text>
+    //   <Text>My Son, JW, Mom and father Love you!!.</Text>
+    //   <Text>Time to study math</Text>
+    //   <Text>When you finish making tea for mom, plz come to study !!!</Text>
+    //   <StatusBar style="auto" />
+    // </View>
+    // <View style={styles.container}>
+    //   <View style={styles.yelloView}>
+    //     {/* <Text>Hello</Text> */}
+    //   </View>
+    //   <View style={styles.blueView}>
+    //     {/* <Text>Hello</Text> */}
+    //   </View>
+    // </View>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
+    // backgroundColor: '#fff',
+    // flexDirection: "row",
+    backgroundColor: "green",
+    // alignItems: 'center',
     justifyContent: 'center',
   },
+  yelloView:
+  {
+    flex: 1,
+    backgroundColor: "yellow"  
+  },
+
+  blueView:
+  {
+    flex: 3,
+    backgroundColor: "blue"
+  },
+
+  text: {
+    color : "white",
+    width : 100
+  }
+
 });
--- README.md
+++ README.md
@@ -1,1 +1,10 @@
 # react-native-forkin-weather
+# SDk : https://docs.expo.io/versions/latest/sdk/overview/
+# https://reactnative.dev/docs/getting-started
+# installation : https://docs.expo.io/get-started/installation/
+  npm install -g expo-cli
+# https://docs.expo.io/get-started/create-a-new-app/
+  expo init react-native-app
+# git remote add origin
+  git pull origin master --allow-unrelated-histories
+# expo login  
