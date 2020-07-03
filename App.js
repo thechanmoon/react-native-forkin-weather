2ed60e3effe3c7fb86a8584d139c6195a8596ad4
chan <thechanmoon@gmail.com>
2020-07-03 02:58:05 -0400

    weather api from api.openweathermap.org

--- App.js
+++ App.js
@@ -1,54 +1,94 @@
-import { StatusBar } from 'expo-status-bar';
+// import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import {Alert} from "react-native";
+// import { StyleSheet, Text, View } from 'react-native';
 import Loading from "./Loading";
+import * as Location from 'expo-location';
+import axios from "axios";
 
 
-export default function App() {
-  return (
-    <Loading> </Loading>
-    // <View style={styles.container}>
-    //   <Text style={styles.text}>Hi JW!!</Text>
-    //   <Text>My Son, JW, Mom and father Love you!!.</Text>
-    //   <Text>Time to study math</Text>
-    //   <Text>When you finish making tea for mom, plz come to study !!!</Text>
-    //   <StatusBar style="auto" />
-    // </View>
-    // <View style={styles.container}>
-    //   <View style={styles.yelloView}>
-    //     {/* <Text>Hello</Text> */}
-    //   </View>
-    //   <View style={styles.blueView}>
-    //     {/* <Text>Hello</Text> */}
-    //   </View>
-    // </View>
-  );
-}
+const API_KEY = "";
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    // backgroundColor: '#fff',
-    // flexDirection: "row",
-    backgroundColor: "green",
-    // alignItems: 'center',
-    justifyContent: 'center',
-  },
-  yelloView:
-  {
-    flex: 1,
-    backgroundColor: "yellow"  
-  },
+export default class extends React.Component {
+  state = {
+    isLoading: true
+  };
 
-  blueView:
-  {
-    flex: 3,
-    backgroundColor: "blue"
-  },
 
-  text: {
-    color : "white",
-    width : 100
+  getWeather = async(latitude,longitude) => {
+    const {data} = await axios.get(`http://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=${API_KEY}`)
+    console.log(data);
   }
 
-});
+  getLocation = async() =>{
+    try{
+      // const response = Location.requestPermissionsAsync();
+      // console.log(response);
+      // throw Error();
+      await Location.requestPermissionsAsync();
+
+      // const {coords} = await Location.getCurrentPositionAsync();
+      // console.log(coords.latitude, coords.longitude);
+      const {coords:{latitude,longitude}} = await Location.getCurrentPositionAsync();
+      this.getWeather(latitude,longitude);
+      this.setState({isLoading:false});
+    }catch(error)
+    {
+      Alert("Request Permission error : ", error)
+    }
+  }
+
+  componentDidMount(){
+    this.getLocation();
+  }
+  render(){
+    const {isLoading} = this.state;
+    return (
+      // isLoading? <Loading/> : null
+      <Loading/>
+      // <View style={styles.container}>
+      //   <Text style={styles.text}>Hi JW!!</Text>
+      //   <Text>My Son, JW, Mom and father Love you!!.</Text>
+      //   <Text>Time to study math</Text>
+      //   <Text>When you finish making tea for mom, plz come to study !!!</Text>
+      //   <StatusBar style="auto" />
+      // </View>
+      // <View style={styles.container}>
+      //   <View style={styles.yelloView}>
+      //     {/* <Text>Hello</Text> */}
+      //   </View>
+      //   <View style={styles.blueView}>
+      //     {/* <Text>Hello</Text> */}
+      //   </View>
+      // </View>
+    );
+  }
+}
+
+// const styles = StyleSheet.create({
+//   container: {
+//     flex: 1,
+//     // backgroundColor: '#fff',
+//     // flexDirection: "row",
+//     backgroundColor: "green",
+//     // alignItems: 'center',
+//     justifyContent: 'center',
+//   },
+//   yelloView:
+//   {
+//     flex: 1,
+//     backgroundColor: "yellow"  
+//   },
+
+//   blueView:
+//   {
+//     flex: 3,
+//     backgroundColor: "blue"
+//   },
+
+//   text: {
+//     color : "white",
+//     width : 100
+//   }
+
+// });
--- README.md
+++ README.md
@@ -2,9 +2,17 @@
 # SDk : https://docs.expo.io/versions/latest/sdk/overview/
 # https://reactnative.dev/docs/getting-started
 # installation : https://docs.expo.io/get-started/installation/
-  npm install -g expo-cli
+  npm install -g expo-cli  
+  npm install axios ( yarn add axios )
+  https://docs.expo.io/versions/latest/sdk/overview/
+  expo install expo-location
 # https://docs.expo.io/get-started/create-a-new-app/
   expo init react-native-app
 # git remote add origin
   git pull origin master --allow-unrelated-histories
-# expo login  
+# expo login 
+  'expo login' in the terminal. 
+# const API_KEY = "";
+
+# currunt location : https://ip-api.com/
+
